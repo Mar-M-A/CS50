084153fabf2635264038502bdf462ed7914bf041
Mar Monter Anduix <marmonteranduix@MacBook-Air-de-Mar.local>
2024-07-26 16:27:48 +0200

    left half-pyramid

---
+++ mario.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+int main()
+{
+    int i, space = 2, j = 0;
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n == 0 || n < 1 || n > 8);
+    for (i = 0; i < n; i++)
+    {
+        for (space = 2; space <= n - i; space++)
+        {
+            printf(" ");
+        }
+        for (j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
